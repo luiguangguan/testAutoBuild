6441e0cfc9159c95bd3d158b0c4d06e887402149
Simon <lvguangguan@live.com>
2023-06-10 12:55:11 +0800

    OK

--- testAutoBuild/Program.cs
+++ testAutoBuild/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace testAutoBuild
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            JsonConvert.SerializeObject(new { a = 1 });
             Console.WriteLine("hello world!");
         }
     }
